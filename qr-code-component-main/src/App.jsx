482b8b3c447b6a20f40489960c246356d0e1b36a
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-27 18:35:37 -0500

    initial solution

--- qr-code-component-main/src/App.jsx
+++ qr-code-component-main/src/App.jsx
@@ -4,19 +4,35 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://frontendmentor.io" target="_blank">
-          <img src={qrCode} className="" alt="QR Code" />
-        </a>
-        {/* Improve your front-end skills by building projects Scan the QR code to visit
-    Frontend Mentor and take your coding skills to the next level
+      <div className="modal-wrapper">
+        <div className="modal">
+          <div className="modal-content">
+            <a href="https://frontendmentor.io" target="_blank">
+              <img src={qrCode} className="qr-code" alt="QR Code" />
+            </a>
+            <div className="content">
+              <p className="title">
+                Improve your front-end skills by building projects
+              </p>
+              <p className="description">
+                Scan the QR code to visit Frontend Mentor and take your coding
+                skills to the next level
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    <div class="attribution">
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-        >Frontend Mentor</a
-      >. Coded by <a href="#">Your Name Here</a>.
-    </div> */}
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/jessesdevaney" target="_blank">
+          Jesse Devaney
+        </a>
+        .
       </div>
     </div>
   );
